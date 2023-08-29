7742a4caaa3a7262b1004400a902d5c874bf640b
Rakesh Sangem <rakeshsangem88@gmail.com>
2023-08-29 23:28:42 +0530

    logging middleware

--- server.js
+++ server.js
@@ -7,20 +7,19 @@ const PORT = 3000;
 const friends = [
   {
     id: 0,
-    name: 'Albert',
+    name: 'Albert Einstein',
   },
   {
     id: 1,
-    name: 'Elon',
-  },
-  {
-    id: 2,
-    name: 'Mark Zukerburg',
+    name: 'Sir Isaac Newton',
   },
 ];
 
-app.get('/', (req, res) => {
-  res.send('Hello, Rakesh!');
+app.use((req, res, next) => {
+  const start = Date.now();
+  next();
+  const delta = Date.now() - start;
+  console.log(`${req.method} ${req.url} ${delta}ms`);
 });
 
 app.get('/friends', (req, res) => {
@@ -30,14 +29,21 @@ app.get('/friends', (req, res) => {
 app.get('/friends/:friendId', (req, res) => {
   const friendId = Number(req.params.friendId);
   const friend = friends[friendId];
-
   if (friend) {
-    res.json(friend);
+    res.status(200).json(friend);
   } else {
     res.status(404).json({
-      error: 'friend does not exist!',
+      error: 'Friend does not exist',
     });
   }
+});
+
+app.get('/messages', (req, res) => {
+  res.send('<ul><li>Helloo Albert!</li></ul>');
+});
+
+app.post('/messages', (req, res) => {
+  console.log('Updating messages...');
 });
 
 app.listen(PORT, () => {
