249450e325d286361980db6e470eef9bf8f95805
Rakesh Sangem <rakeshsangem88@gmail.com>
2023-08-30 21:20:49 +0530

    chore: added routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
-const messagesController = require('./controllers/messages.controller');
-const friendsController = require('./controllers/friends.controller');
+const friendsRouter = require('./routes/friends.router');
+const messagesRouter = require('./routes/messages.router');
 
 const app = express();
 
@@ -11,17 +11,13 @@ app.use((req, res, next) => {
   const start = Date.now();
   next();
   const delta = Date.now() - start;
-  console.log(`${req.method} ${req.url} ${delta}ms`);
+  console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
 
 app.use(express.json());
 
-app.get('/friends', friendsController.getFriends);
-app.post('/friends', friendsController.postFriend);
-app.get('/friends/:friendId', friendsController.getFriend);
-
-app.get('/messages', messagesController.getMessages);
-app.post('/messages', messagesController.postMessages);
+app.use('/friends', friendsRouter);
+app.use('/messages', messagesRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}...`);
